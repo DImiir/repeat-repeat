d1efed03e567d7addae78a2495dbced302d9b77a
DImiir <d.i.mironenko@yandex.ru>
2024-11-26 22:35:39 +0300

    доделал пагинацию (версия 1.0). далее в планах сделать fsm или улучшить пагинацию.

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+import languages
 
 
-# строитель клавиатур
 def keyboard_maker(data: list[str], width: int, one_time: bool = False):
     kb = ReplyKeyboardBuilder()
     buttons = [KeyboardButton(text=text) for text in data]
@@ -17,10 +17,14 @@ def keyboard_maker(data: list[str], width: int, one_time: bool = False):
     return keyboard
 
 
-def inline_pagination_maker(number: int, amount: int):
+def inline_dictionary_keyboard_maker(items: list[str], page: int, amount: int):
     kb = InlineKeyboardBuilder()
+
+    for item in items:
+        kb.row(InlineKeyboardButton(text=languages.lexicon[item], callback_data=f'dictionary_{item}'))
+
     buttons = [InlineKeyboardButton(text='<-', callback_data='previous'),
-               InlineKeyboardButton(text=f'{number}/{amount}', callback_data='page'),
+               InlineKeyboardButton(text=f'{page}/{amount}', callback_data='page'),
                InlineKeyboardButton(text='->', callback_data='next'),
                InlineKeyboardButton(text='Добавить слово', callback_data='choose_language')]
     kb.row(*buttons, width=3)
@@ -32,7 +36,7 @@ def inline_language_keyboard_maker(items: list[tuple], page: int, amount: int):
     kb = InlineKeyboardBuilder()
 
     for i in items:
-        kb.row(InlineKeyboardButton(text=f'{i[0]}', callback_data=f'language_{i[1]}'), width=1)
+        kb.row(InlineKeyboardButton(text=f'{i[1]}', callback_data=f'language_{i[0]}'), width=1)
 
     if page == 1:
         kb.row(*[InlineKeyboardButton(text=f'{page}/{amount}', callback_data='page'),
@@ -48,8 +52,28 @@ def inline_language_keyboard_maker(items: list[tuple], page: int, amount: int):
     return kb.as_markup()
 
 
+def inline_make_dictionary():
+    kb = InlineKeyboardBuilder()
+    kb.row(InlineKeyboardButton(text='Добавить слово', callback_data='choose_language'))
+    return kb.as_markup()
+
+
+def inline_words_keyboard_maker(lang: str, page: int, amount: int):
+    kb = InlineKeyboardBuilder()
+    if page == 1:
+        kb.row(*[InlineKeyboardButton(text=f'{page}/{amount}', callback_data='page'),
+                 InlineKeyboardButton(text='->', callback_data=f'next_page_words_{lang}_{page}')])
+    elif page == amount:
+        kb.row(*[InlineKeyboardButton(text='<-', callback_data=f'previous_page_words_{lang}_{page}'),
+                 InlineKeyboardButton(text=f'{page}/{amount}', callback_data='page')])
+    else:
+        kb.row(*[InlineKeyboardButton(text='<-', callback_data=f'previous_page_words_{lang}_{page}'),
+                 InlineKeyboardButton(text=f'{page}/{amount}', callback_data='page'),
+                 InlineKeyboardButton(text='->', callback_data=f'next_page_words_{lang}_{page}')])
+    kb.row(InlineKeyboardButton(text='Добавить слово', callback_data='choose_language'))
+    return kb.as_markup()
+
+
 keyboard_menu = keyboard_maker(['Помощь ❓', 'Открыть словарь 📚', 'Начать тестирование 🎓'], 3)
 
-keyboard_dict = inline_pagination_maker(1, 1)
-
-
+new_dictionary = inline_make_dictionary()
--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from aiogram.types import Message, CallbackQuery
 from aiogram import F
 
 from database.models import UserORM, DictionaryORM
-from keyboards import keyboard_menu, keyboard_dict, inline_language_keyboard_maker
+from keyboards import (keyboard_menu, inline_language_keyboard_maker, inline_dictionary_keyboard_maker,
+                       new_dictionary, inline_words_keyboard_maker)
 from database import db_session
 
 import languages
 
-BOT_TOKEN = '7855547856:AAEUnPMKytMhnrSnwYFj98i1q3lTyCdPkbI'
+from math import ceil
+
+BOT_TOKEN = 'BOT_TOKEN'
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
@@ -55,36 +58,92 @@ async def user_help_command(message: Message):
 
 @dp.message(Command(commands=["dict"]))
 @dp.message(F.text.in_(['Открыть словарь 📚', 'Открыть словарь', 'открыть словарь']))
-async def open_dictionary_command(message: Message):
+async def choose_dictionary_command(message: Message):
     session = db_session.create_session()
     data = session.query(UserORM).filter(UserORM.tg_id == message.model_dump()['from_user']['id']).one()
-    text = [i.word for i in data.dictionary]
-    translated = [i.translated_word for i in data.dictionary]
-    final_text = '\n'.join([f'{text[i]} - {translated[i]}' for i in range(len(text))])
-    await message.answer(f'''
-Вот Ваш словарь.
+    langs = sorted(list({i.language for i in data.dictionary}))
+    if langs:
+        m = len(langs)
+        amount = ceil(m / 10)
+        n = m if m < 10 else 10
+        await message.answer(f'''
+Вот все Ваши словари.
+''', reply_markup=inline_dictionary_keyboard_maker(langs[:n], 1, amount))
+    else:
+        await message.answer(f'''
+У Вас нет словарей.
+''', reply_markup=new_dictionary)
+
+
+@dp.callback_query(F.data.startswith('dictionary_'))
+async def open_dictionary_command(callback: CallbackQuery):
+    session = db_session.create_session()
+    data = session.query(UserORM).filter(UserORM.tg_id == callback.model_dump()['from_user']['id']).one()
+    lang = callback.data.lstrip('dictionary_')
+    text = [(i.word, i.translated_word) for i in data.dictionary if i.language == lang]
+    m = len(text)
+    amount = ceil(m / 10)
+    n = m if m < 10 else 10
+    final_text = '\n'.join([f'{i[0]} - {i[1]}' for i in text[:n]])
+    await callback.message.edit_text(f'''
 {final_text}
-''', reply_markup=keyboard_dict)
+''', reply_markup=inline_words_keyboard_maker(lang, 1, amount))
+
+
+@dp.callback_query(F.data.startswith('next_page_words_'))
+async def previous_page_words_command(callback: CallbackQuery):
+    page = int(callback.data.split('_')[-1])
+    lang = callback.data.split('_')[-2]
+
+    session = db_session.create_session()
+    data = session.query(UserORM).filter(UserORM.tg_id == callback.model_dump()['from_user']['id']).one()
+    text = [(i.word, i.translated_word) for i in data.dictionary if i.language == lang]
+    amount = ceil(len(text) / 10)
+
+    if page * 10 < len(text):
+        final_text = '\n'.join([f'{i[0]} - {i[1]}' for i in text[page * 10:]])
+    else:
+        final_text = '\n'.join([f'{i[0]} - {i[1]}' for i in text[page * 10: (page + 1) * 10]])
+
+    await callback.message.edit_text(f'''
+{final_text}
+''', reply_markup=inline_words_keyboard_maker(lang, page + 1, amount))
+
+
+@dp.callback_query(F.data.startswith('previous_page_words_'))
+async def previous_page_words_command(callback: CallbackQuery):
+    page = int(callback.data.split('_')[-1])
+    lang = callback.data.split('_')[-2]
+
+    session = db_session.create_session()
+    data = session.query(UserORM).filter(UserORM.tg_id == callback.model_dump()['from_user']['id']).one()
+    text = [(i.word, i.translated_word) for i in data.dictionary if i.language == lang]
+    final_text = '\n'.join([f'{i[0]} - {i[1]}' for i in text[(page - 2) * 10: (page - 1) * 10]])
+    amount = ceil(len(text) / 10)
+
+    await callback.message.edit_text(f'''
+{final_text}
+''', reply_markup=inline_words_keyboard_maker(lang, page - 1, amount))
 
 
 @dp.callback_query(F.data == 'choose_language')
-async def choose_language_dictionary_command(callback: CallbackQuery, state: FSMContext):
-    amount = round(len(languages.items) // 10)
+async def choose_language_add_word_command(callback: CallbackQuery, state: FSMContext):
+    amount = ceil(len(languages.items) / 10)
 
+    await state.set_state(FSMMachine.choose)
     await callback.message.edit_text('''
 Какой язык ?
 ''', reply_markup=inline_language_keyboard_maker(languages.items[:10], 1, amount))
-    await state.set_state(FSMMachine.choose)
 
 
 @dp.callback_query(StateFilter(FSMMachine.choose), F.data.startswith('next_page_lang'))
 async def next_page_lang_command(callback: CallbackQuery):
-    amount = round(len(languages.items) // 10)
+    amount = ceil(len(languages.items) / 10)
 
     page = int(callback.data.lstrip('next_page_lang'))
 
     if page * 10 >= len(languages.items):
-        items = languages.items[page * 10: len(languages.items)]
+        items = languages.items[page * 10:]
     else:
         items = languages.items[page * 10: (page + 1) * 10]
 
@@ -95,7 +154,7 @@ async def next_page_lang_command(callback: CallbackQuery):
 
 @dp.callback_query(StateFilter(FSMMachine.choose), F.data.startswith('previous_page_lang'))
 async def next_page_lang_command(callback: CallbackQuery):
-    amount = round(len(languages.items) // 10)
+    amount = ceil(len(languages.items) / 10)
 
     page = int(callback.data.lstrip('previous_page_lang'))
 
