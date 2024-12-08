560a9cdd61df9064e07751e3b0fc4315512e40b4
DImiir <d.i.mironenko@yandex.ru>
2024-12-08 20:13:28 +0300

    Удалён callback_factory.

--- callback_factory.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-
-class CallbackFactory(CallbackData, prefix='cb', sep='.'):
-    other_days: str
-    day: str
-    action_id: int
-    delete: int
-    edit: int
